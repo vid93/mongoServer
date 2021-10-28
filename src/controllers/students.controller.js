eefc3eff42e5de59b24de3a34a13e77ee39b7b71
Vid <vidmiljanovic@gmail.com>
2021-10-28 19:11:49 +0200

    my first database

---
+++ src/controllers/students.controller.js
@@ -0,0 +1,66 @@
+import Student from '../models/student.model';
+import _ from 'lodash';
+
+const create = (req, res) => {
+    const student = Student(req.body);
+    student.save((err, data) => {
+        if(err){
+            return res.status(400).json(err.message)
+        }
+        res.status(201).json(data);
+    });
+}
+
+const list = (req, res) => {
+    Student.find((err, data) => {
+        if(err){
+            return res.status(400).json(err.message)
+        }
+        res.status(200).json(data);
+    });
+}
+
+const read = (req, res) => {
+    const id = req.params.id;
+    Student.findById(id).exec((err, data) => {
+        if(err){
+            return res.status(400).json(err.message)
+        }
+        res.status(200).json(data);
+    });
+}
+
+const update = (req, res) => {
+    const id = req.params.id;
+    Student.findById(id).exec((err, data) => {
+        if(err || !data){
+            return res.status(400).json('Student not found!')
+        }
+        const student = _.extend(data, req.body);
+        student.save((err, data) => {
+            if(err){
+                return res.status(400).json(err.message)
+            }
+            res.status(200).json(data);
+        })
+
+        res.status(200).json(data);
+    })
+}
+
+const remove = (req, res) => {
+    const id = req.params.id;
+    Student.findById(id).exec((err, data) => {
+        if(err || !data){
+            return res.status(400).json('Student not found!')
+        }
+        data.remove((err, data) => {
+            if(err){
+                return res.status(400).json(err.message);
+            }
+            res.status(200).json('Student deleted.')
+        })
+    })
+}
+
+export default { list, remove, update, create, read }
